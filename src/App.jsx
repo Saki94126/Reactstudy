5456b2bbe0285db3aca5da73ea830d82aeb1a216
Saki94126 <104111843+Saki94126@users.noreply.github.com>
2022-05-12 13:45:38 +0900

    propsとuseEffectの学習

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+/* eslint react-hooks/exhaustive-deps : off*/
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+  const onClickSwichShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか?</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ！</button>
-      <button>on/off</button>
+      <br />
+      <button onClick={onClickSwichShowFlag}>on/off</button>
       <p>{num}</p>
-      <p>(^^)/</p>
+      {faceShowFlag && <p>(^^)/</p>}
     </>
   );
 };
